7046c35fe4995b5d0ff367a964f5cc7bddb550e5
Karen <karen.rodriguez@bg.com.bo>
2025-10-22 12:11:17 -0400

    init

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +21,23 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+// your first API endpoint... 
+app.get("/api/:date?", function (req, res) {
+  console.log("params", req.params);
+  
+  const date = new Date(parseInt(req.params.date))
+  console.log("D", date);
+  
+  // if (!date) return
 
+  res.json({ unix: req.params.date, utc: date });
+
+});
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3001, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
